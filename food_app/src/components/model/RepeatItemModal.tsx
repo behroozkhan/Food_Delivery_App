0ef58d14fcc92445569e7dc6766380876978e067
behrooz khan <kbalochkhan67@gmail.com>
2025-07-22 16:27:45 +0500

    some feature add in RepeatItemModal Components

--- food_app/src/components/model/RepeatItemModal.tsx
+++ food_app/src/components/model/RepeatItemModal.tsx
@@ -1,10 +1,60 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
+import React, { FC, useEffect } from 'react'
+import { useAppSelector } from '@state/reduxHook';
+import { selectRestaurantCartItem } from '@state/reducers/CartSlice';
+import { useStyles } from 'react-native-unistyles';
+import { modelStyles } from '@unistyles/modelStyles';
+import CustomText from '@components/global/CustomText';
+import { Colors } from '@unistyles/Constants';
 
-const RepeatItemModal = () => {
+const RepeatItemModal: FC<{
+  item: any;
+  restaurant: any;
+  onOpenAddModal: () => void;
+  closeModal: () => void;
+}> = ({ item, onOpenAddModal, restaurant, closeModal }) => {
+  const cartItem = useAppSelector(
+    selectRestaurantCartItem(restaurant?.id, item?.id),
+  );
+
+  const { styles } = useStyles(modelStyles);
+
+
+  useEffect(() => {
+    if (!cartItem) {
+      closeModal();
+    }
+  }, [cartItem])
+
   return (
     <View>
-      <Text>RepeatItemModal</Text>
+      <View style={styles.noShadowHeaderContainer}>
+        <View style={styles.flexRowGap}>
+          <CustomText fontFamily="Okra-Bold" fontSize={13}>
+            Repeat last used customization?
+          </CustomText>
+        </View>
+      </View>
+
+      <ScrollView
+        contentContainerStyle={
+          styles.scrollContainerWhiteBackground
+        }>
+
+      </ScrollView>
+
+
+      <View style={styles.noShadowFooterContainer}>
+        <TouchableOpacity onPress={onOpenAddModal}>
+          <CustomText
+            fontFamily="Okra-Bold"
+            color={Colors.active}
+            fontSize={11}>
+            + Add new customization
+          </CustomText>
+        </TouchableOpacity>
+      </View>
+
     </View>
   )
 }
